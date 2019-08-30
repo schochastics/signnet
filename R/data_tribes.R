3b9f123e7bcba2deecc640108709e8df8e50fdf4
schochastics <david@schochastics.net>
2019-08-30 15:19:09 +0100

    updated docs

--- R/data_tribes.R
+++ R/data_tribes.R
@@ -1,7 +1,8 @@
-#' Florentine family marriage network
-#'
-#' @format An igraph graph containing positive/negative ties among New Guinean highland tribes.
-#' @description The data was downloaded from http://vlado.fmf.uni-lj.si/pub/networks/data/ucinet/gama.dat
+#' Signed network of New Guinean highland tribes.
+#' @description Signed social network of tribes of the Gahuku–Gama alliance structure of the Eastern Central Highlands of New Guinea, from Kenneth Read.
+#' The network contains sixteen tribes connected by friendship ("rova") and enmity ("hina").
+#' @format An igraph object
+#' @source http://vlado.fmf.uni-lj.si/pub/networks/data/ucinet/gama.dat
 #' @references Read, K. E. (1954) Cultures of the central highlands, New Guinea. *Southwestern Journal of Anthropology*, 1–43.
 "tribes"
 
