a8a3d5f56e769c9d0415405be6278e10d3268fbc
schochastics <david@schochastics.net>
2019-11-27 13:23:25 +0000

    added triangle listing (fixes #2)

--- R/signed_triangles.R
+++ R/signed_triangles.R
@@ -4,13 +4,14 @@
 #' @param g signed network.
 #' @return counts for all 4 signed triangle types
 #' @author David Schoch
+#' @seealso [signed_triangles]
 #' @examples
 #' library(igraph)
 #' g <- graph.full(4)
 #' E(g)$sign <- c(-1,1,1,-1,-1,1)
-#' signed_triangles(g)
+#' count_signed_triangles(g)
 #' @export
-signed_triangles <- function(g){
+count_signed_triangles <- function(g){
   if(!"sign"%in%igraph::edge_attr_names(g)){
     stop("network does not have a sign edge attribute")
   }
@@ -53,4 +54,48 @@ signed_triangles <- function(g){
 
   tri_counts[match(names(tmp_counts),names(tri_counts))] <- tmp_counts
   tri_counts
+}
+
+#' @title list signed triangles
+#' @description lists all possible signed triangles
+#'
+#' @param g signed network.
+#' @return matrix of vertex ids and the number of positive ties per triangle
+#' @author David Schoch
+#' @seealso [count_signed_triangles]
+#' @examples
+#' library(igraph)
+#' g <- graph.full(4)
+#' E(g)$sign <- c(-1,1,1,-1,-1,1)
+#' signed_triangles(g)
+#' @export
+signed_triangles <- function(g){
+  if(!"sign"%in%igraph::edge_attr_names(g)){
+    stop("network does not have a sign edge attribute")
+  }
+  if(igraph::is.directed(g)){
+    stop("g must be undirected")
+  }
+  eattrV <- igraph::get.edge.attribute(g,"sign")
+  if(!all(eattrV%in%c(-1,1))){
+    stop("sign may only contain -1 and 1")
+  }
+
+  tmat <- t(matrix(igraph::triangles(g),nrow=3))
+  if(nrow(tmat)==0){
+    warning("g does not contain any triangles")
+    return(NULL)
+  }
+  emat <- t(apply(tmat,1,function(x) c(igraph::get.edge.ids(g,x[1:2]),
+                                       igraph::get.edge.ids(g,x[2:3]),
+                                       igraph::get.edge.ids(g,x[c(3,1)]))))
+
+  semat <- matrix(0,nrow(emat),3)
+  semat[,1] <- eattrV[emat[,1]]
+  semat[,2] <- eattrV[emat[,2]]
+  semat[,3] <- eattrV[emat[,3]]
+  cls <- apply(semat, 1, function(v) length(which(v==1)))
+  emat <- cbind(emat,unname(cls))
+  colnames(emat) <- c("V1","V2","V3","P")
+  emat
 }
