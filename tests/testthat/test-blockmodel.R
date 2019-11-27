eefe1181824110cd471cc3a240d56581de5c5acf
schochastics <david@schochastics.net>
2019-11-27 13:30:51 +0000

    added blockmodel tests

--- tests/testthat/test-blockmodel.R
+++ tests/testthat/test-blockmodel.R
@@ -8,3 +8,16 @@ test_that("blockmodeling k error works", {
   data("tribes")
   expect_error(signed_blockmodel(tribes))
 })
+
+test_that("general blockmodeling works", {
+  data("tribes")
+  clu <- signed_blockmodel_general(tribes,blockmat = matrix(c(1,-1,-1,
+                                                              -1,1,-1,
+                                                              -1,-1,1),3,3,byrow = T))
+  expect_equal(max(clu$membership),3)
+})
+
+test_that("general  blockmodeling k error works", {
+  data("tribes")
+  expect_error(signed_blockmodel(tribes))
+})
