260af127714dbddde98adb6fc5511a2799fd4384
schochastics <david@schochastics.net>
2022-11-04 08:06:56 +0100

    added github LICENSE and tests

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,18 @@
+test_that("from_adjacency throws error", {
+  A <- matrix(c(0,2,-2,1,0,1,-1,1,0),3,3)
+  expect_error(graph_from_adjacency_matrix_signed(A))
+})
+
+test_that("from_edgelist works",{
+  el <- matrix(c("foo", "bar", "bar", "foobar"), ncol = 2, byrow = TRUE)
+  signs <- c(-1, 1)
+  g <- graph_from_edgelist_signed(el, signs)
+  expect_equal(signs,igraph::E(g)$sign)
+  expect_equal(igraph::ecount(g),2)
+})
+
+test_that("from_edgelist throws error",{
+  el <- matrix(c("foo", "bar", "bar", "foobar"), ncol = 2, byrow = TRUE)
+  signs <- c(-1, 2)
+  expect_Error(graph_from_edgelist_signed(el, signs))
+})
